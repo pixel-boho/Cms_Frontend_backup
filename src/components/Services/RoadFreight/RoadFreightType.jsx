884b3df39b7ee75454e70e1971057f355aa0a15c
Ajay Renjith <KFITOFFICE@gmail.com>
2023-10-20 23:52:12 +0530

    road freight jsx update

--- src/components/Services/RoadFreight/RoadFreightType.jsx
+++ src/components/Services/RoadFreight/RoadFreightType.jsx
@@ -1,189 +1,180 @@
 import React from "react";
 import { Container, Row, Col, Card } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./../../../css/ServicesCss/RoadFreight/RoadFreightType.css";
+import "./../../../css/ServicesCss/Clearance/ClearanceType.css";
 
 function RoadFreightType() {
   return (
     <>
-      <Container fluid className="container-rfgt-type-serv">
-        <h3 className="main-title-rfgt-type-serv">
-          Types of Road Freight <br />
+      <Container fluid className="position-relative">
+        <div className="container-cc-type-serv">
+          <h3 className="main-title-cc-type-serv">
+          Types of Road Freight
           Services We Offer
-        </h3>
+          </h3>
+        </div>
         <img
           className="bg-main-img"
           src="images/CargoService/cargo-mixed.png"
           alt=""
         />
-        <Row className="card-rfgt-type-serv">
-          <Col md={4}>
-            <Card className="card-child">
-              <Card.Img
-                src="images/RoadFreight/FullTruckLoad.jpg"
-                className="rfgt-type-serv-general"
-              />
-              <Card.Body>
-                <Card.Title>
-                  Full Truck Load (FTL)
-                  <br />
-                  <br />
-                </Card.Title>
-                <Card.Text></Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col md={4}>
-            <Card className="card-child">
-              <Card.Img
-                src="images/RoadFreight/LessthanTruckload.jpg"
-                className="rfgt-type-serv-general"
-              />
-              <Card.Body>
-                <Card.Title>Less than Truckload (LTL)</Card.Title>
-                <Card.Text></Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col md={4}>
-            <Card className="card-child">
-              <Card.Img
-                src="images/RoadFreight/DomesticTransportation.jpg"
-                className="rfgt-type-serv-general"
-              />
-              <Card.Body>
-                <Card.Title>Domestic Transportation</Card.Title>
-                <Card.Text></Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-        <Row className="card-rfgt-type-serv">
-          <Col md={4}>
-            <Card className="card-child">
-              <Card.Img
-                src="images/RoadFreight/GCCTransportation.jpg"
-                className="rfgt-type-serv-general"
-              />
-              <Card.Body>
-                <Card.Title>
-                  GCC Transportation
-                  <br />
-                  <br />
-                </Card.Title>
-                <Card.Text></Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col md={4}>
-            <Card className="card-child">
-              <Card.Img
-                src="images/RoadFreight/HeavyHaulage.jpg"
-                className="rfgt-type-serv-general"
-              />
-              <Card.Body>
-                <Card.Title>
-                  Heavy Haulage
-                  <br />
-                  <br />
-                </Card.Title>
-                <Card.Text></Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col md={4}>
-            <Card className="card-child">
-              <Card.Img
-                src="images/RoadFreight/SpecialEquipmentRental.jpg"
-                className="rfgt-type-serv-general"
-              />
-              <Card.Body>
-                <Card.Title>Special Equipment Hire and Rental</Card.Title>
-                <Card.Text></Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
+        <div className="container-cc-type-serv">
+          <div className="card-cc-type-serv row">
+            <Col md={4}>
+              <Card className="card-child-service">
+                <Card.Img
+                  src="images/RoadFreight/FullTruckLoad.jpg"
+                  className="cc-type-serv-general"
+                />
+                <Card.Body>
+                  <Card.Title>Full Truck Load (FTL)</Card.Title>
+                  <Card.Text></Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col md={4}>
+              <Card className="card-child-service">
+                <Card.Img
+                  src="images/RoadFreight/LessthanTruckload.jpg"
+                  className="cc-type-serv-general"
+                />
+                <Card.Body>
+                  <Card.Title>Less than Truckload (LTL)</Card.Title> 
+                  <Card.Text></Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col md={4}>
+              <Card className="card-child-service">
+                <Card.Img
+                  src="images/RoadFreight/DomesticTransportation.jpg"
+                  className="cc-type-serv-general"
+                />
+                <Card.Body>
+                  <Card.Title>Domestic Transportation</Card.Title>
+                  <Card.Text></Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </div>
+        </div>
+        <div className="container-cc-type-serv">
+          <div className="card-cc-type-serv row">
+            <Col md={4}>
+              <Card className="card-child-service">
+                <Card.Img
+                  src="images/RoadFreight/GCCTransportation.jpg"
+                  className="cc-type-serv-general"
+                />
+                <Card.Body>
+                  <Card.Title>
+                  GCC Transportation 
+                 
+                 
+                  </Card.Title>
+                  <Card.Text></Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col md={4}>
+              <Card className="card-child-service">
+                <Card.Img
+                  src="images/RoadFreight/HeavyHaulage.jpg"
+                  className="cc-type-serv-general"
+                />
+                <Card.Body>
+                  <Card.Title> Heavy Haulage</Card.Title>
+                  <Card.Text></Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col md={4}>
+              <Card className="card-child-service">
+                <Card.Img
+                  src="images/RoadFreight/SpecialEquipmentRental.jpg" 
+                  className="cc-type-serv-general"
+                />
+                <Card.Body>
+                  <Card.Title>
+                  Special Equipment Hire and Rental 
+                  </Card.Title>
+                  <Card.Text></Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </div>
+        </div>
       </Container>
 
-      <Container fluid className="container-rfgt-type-serv">
+
+      <Container fluid className="position-relative">
         <img
-          className="bg-main-img-one"
-          src="images/CargoService/cargo-mixed.png"
+          className="bg-main-img"
+          src="images/CargoService/cargo-mixed.png" 
           alt=""
         />
-        <Row className="card-rfgt-type-serv">
-          <Col md={4}>
-            <Card className="card-child">
-              <Card.Img
-                src="images/RoadFreight/MallDeliveries.jpg"
-                className="rfgt-type-serv-general"
-              />
-              <Card.Body>
-                <Card.Title>
-                  Mall Deliveries
-                  <br />
-                  <br />
-                  <br />
-                  <br />
-                  <br />
-                  <br />
-                </Card.Title>
-                <Card.Text></Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col md={4}>
-            <Card className="card-child">
-              <Card.Img
-                src="images/RoadFreight/TemperatureControlled.jpg"
-                className="rfgt-type-serv-general"
-              />
-              <Card.Body>
-                <Card.Title>
-                  Temperature Controlled
-                  <br />
-                  <br />
-                  <br />
-                  <br />
-                  <br />
-                </Card.Title>
-                <Card.Text></Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col md={4}>
-            <Card className="card-child">
-              <Card.Img
-                src="images/RoadFreight/DangerousGoodsTransportation –usingChemical.jpg"
-                className="rfgt-type-serv-general"
-              />
-              <Card.Body>
-                <Card.Title>
-                  Dangerous Goods Transportation – using Chemical Approved /
-                  Civil Defense Approved Trucks
-                </Card.Title>
-                <Card.Text></Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-        <Row className="card-rfgt-type-serv">
-          <Col md={4}>
-            <Card className="card-child">
-              <Card.Img
-                src="images/RoadFreight/OilfieldDeliveriesandBPSpecTrucks.jpg"
-                className="rfgt-type-serv-general"
-              />
-              <Card.Body>
-                <Card.Title>
+        <div className="container-cc-type-serv">
+          <div className="card-cc-type-serv row">
+            <Col md={4}>
+              <Card className="card-child-service">
+                <Card.Img
+                  src="images/RoadFreight/MallDeliveries.jpg" 
+                  className="cc-type-serv-general"
+                />
+                <Card.Body>
+                  <Card.Title>Mall Deliveries</Card.Title>
+                  <Card.Text></Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col md={4}>
+              <Card className="card-child-service">
+                <Card.Img
+                  src="images/RoadFreight/TemperatureControlled.jpg"
+                  className="cc-type-serv-general"
+                />
+                <Card.Body>
+                  <Card.Title>Temperature Controlled</Card.Title>
+                  <Card.Text></Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col md={4}>
+              <Card className="card-child-service">
+                <Card.Img
+                  src="images/RoadFreight/DangerousGoodsTransportation –usingChemical.jpg"
+                  className="cc-type-serv-general"
+                />
+                <Card.Body>
+                  <Card.Title> Dangerous Goods Transportation – using Chemical Approved /
+                  Civil Defense Approved Trucks</Card.Title> 
+                  <Card.Text></Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </div>
+        </div>
+        <div className="container-cc-type-serv">
+          <div className="card-cc-type-serv row">
+            <Col md={4}>
+              <Card className="card-child-service">
+                <Card.Img
+                  src="images/RoadFreight/OilfieldDeliveriesandBPSpecTrucks.jpg" 
+                  className="cc-type-serv-general"
+                />
+                <Card.Body>
+                  <Card.Title>
                   Oilfield Deliveries – using PDO, Oxy and BP Spec Trucks
-                </Card.Title>
-                <Card.Text></Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
+                 
+                 
+                  </Card.Title>
+                  <Card.Text></Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+
+          </div>
+        </div>
       </Container>
     </>
   );
